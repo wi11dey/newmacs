6379f74cd0a3541da777ce9b0f20a9a18e95b288
Will Dey <wdey@palantir.com>
2026-07-14 16:26:52 +0100

    more

--- src/app/main/core.cljs
+++ src/app/main/core.cljs
@@ -1,19 +1,12 @@
 (ns app.main.core
   (:require ["electron" :refer [app BrowserWindow crashReporter]]))
 
-(def main-window (atom nil))
-
-(defn init-browser []
-  (reset! main-window (BrowserWindow.
-                        (clj->js {:width 800
-                                  :height 600
-                                  :webPreferences
-                                  {:nodeIntegration true
-                                   :contextIsolation false}})))
-  ; Path is relative to the compiled js file (main.js in our case)
-  (.loadURL ^js/electron.BrowserWindow @main-window (str "file://" js/__dirname "/public/index.html"))
-  (.on ^js/electron.BrowserWindow @main-window "closed" #(reset! main-window nil)))
-
 (defn main []
   (.on app "window-all-closed" #(.quit app))
-  (.on app "ready" init-browser))
+  (.on app "ready" #(.loadURL (BrowserWindow.
+                               (clj->js {:width 800
+                                         :height 600
+                                         :webPreferences
+                                         {:nodeIntegration true
+                                          :contextIsolation false}}))
+                              (str "file://" js/__dirname "/public/index.html"))))
